f52c10dc8cb28cfd46f0f577f806d372e27471d5
AlaevSS <sergeyalaev7@gmail.com>
2021-02-28 16:03:16 +0200

    Сверстал и адаптировал шапку

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(function () {
+    $('.mini-menu').click(function () {
+        if ($('nav ul').css('display') == 'none') {
+            $('nav ul').slideDown();
+        } else {
+            $('nav ul').slideUp();
+        }
+        
+    });
+});
